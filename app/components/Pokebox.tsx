ead1c5a04575c4df2745746f69a647ff0f9ad426
janpatmat <patmatayabas@gmail.com>
2025-11-01 14:48:42 +0800

    feat: added dynamic pokecard change

--- app/components/Pokebox.tsx
+++ app/components/Pokebox.tsx
@@ -1,39 +1,59 @@
+"use client"
+
 import React, { use } from 'react'
 import { useState, useEffect } from 'react'
 import Image from 'next/image';
 import Movecards from './Movecards';
-const Pokebox = () => {
+import data from '../data/gymleadertest.json';
+interface PokeStats {
+    id: number | null;
+    name: string | null;
+
+}
+
+const Pokebox: React.FC<PokeStats> = ({id, name}) => {
 const [pokemon, setPokemon] = useState<any>(null);
 const [loading, setLoading] = useState(true);
+
+const leaderData = data.find(x => x.id === id);
+const leaderPokemon = leaderData?.pokemon;
+
 
 useEffect(() => {
     async function fetchData() {
 //    Challenge use .then() instead of async/await
-         const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name?.toLowerCase()}`);
         
         if (response.ok) {
             
             const data = await response.json();
-            console.log(data.types[0].type.name);
+            console.log(data.types);
             setPokemon(data);
         }
        
     }
     fetchData();
     
-    console.log("hello world")
+   
     
-},[])  
+},[name])  
+
+
   return (
     <div className='flex flex-col bg-normalGreyCard w-md rounded-xl border-4 border-black'>
         {/* upper */}
         <div className='flex justify-between p-2 border-b-2 border-black'>
+
+            <h3>{id}</h3>
             
-            <h3 className='uppercase'>{pokemon ? (pokemon.name) : (<p>Loading...</p>)}</h3>
+            <h3 className='uppercase'>{name ? (name) : (<p>Loading...</p>)}</h3>
 
             <div className='flex gap-3'>
                 <h3 >Level</h3>
-                <h3 className='uppercase'>{pokemon ? (pokemon.types[0].type.name) : (<p>Loading...</p>)}</h3>
+                
+                {pokemon?.types?.map((type: {type:{name:string}}) => (
+                    <p className='uppercase'>{type.type.name}</p>
+                ))}
                
             </div>
         </div>
@@ -48,6 +68,7 @@ useEffect(() => {
                 {/* right */}
             <div className='w-1/2'>
                <Movecards name='Transform' type='Normal'/>
+              
             </div>
         </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,29 +6,43 @@ import data from "./data/gymleadertest.json";
 import Pokebox from "./components/Pokebox";
 
 export default function Home() {
-  // Track which box is active (store its id or name)
-  const [activeLeader, setActiveLeader] = useState<string | null>("Brock");
+  
+  const [activeLeader, setActiveLeader] = useState<number | null>(2);
  
-  const handleClick = (leaderName: string) => {
-    setActiveLeader(leaderName);
+  const handleClick = (leaderid: number) => {
+    setActiveLeader(leaderid);
   };
+
+  const leaderData = data.find(x => x.id === activeLeader);
+  const leaderPokemon = leaderData?.pokemon;
 
   return (
     <div>
     <div className="flex flex-row gap-5 p-5">
       {data.map((leader) => (
         <Charbox
-          key={leader.name}
+          key={leader.id}
           gymLeader={leader.image}
           name={leader.name}
-          isActive={activeLeader === leader.name}
-          onClick={() => handleClick(leader.name)}
+          isActive={activeLeader === leader.id}
+          onClick={() => handleClick(leader.id)}
         />
       ))}
 
       
     </div>
-    <Pokebox />
+  
+    
+      
+     {leaderPokemon?.map((leaderpokemon) => (
+  <Pokebox
+    key={`${activeLeader}-${leaderpokemon.pokemonName}`}
+    id={activeLeader}
+    name={leaderpokemon.pokemonName}
+  />
+))}
+
+    
     </div>
   );
 }
